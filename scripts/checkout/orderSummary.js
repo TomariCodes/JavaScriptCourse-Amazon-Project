88889d2470ade29c35f3a12e8094cf44194829b3
Tomari <tomari7878@gmail.com>
2024-09-07 16:01:16 -0500

    created automated tests for formatCurrency

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -10,12 +10,6 @@ import {
 } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 
-/*hello();
-const today = dayjs();
-const deliveryDate = today.add(7, "days");
-console.log(deliveryDate.format("dddd, MMMM D"));
-*/
-
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
 
