f851607eeaa96be860e12a0a2dae0b016fd9e33f
Tomari <tomari7878@gmail.com>
2024-09-10 21:58:04 -0500

    Started running tests for renderorderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -27,6 +27,7 @@ export function renderOrderSummary() {
     const dateString = deliveryDate.format("dddd, MMMM D");
 
     cartSummaryHTML += `<div class="cart-item-container 
+         js-cart-item-container
          js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
             Delivery date: ${dateString}
@@ -43,7 +44,7 @@ export function renderOrderSummary() {
                 <div class="product-price">
                 $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                 <span>
                 Quantity: <span class="quantity-label">${
                   cartItem.quantity
@@ -110,7 +111,7 @@ ${deliveryOptionsHTML(matchingProduct, cartItem)}
     link.addEventListener("click", () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-renderPaymentSummary();
+      renderPaymentSummary();
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`
       );
