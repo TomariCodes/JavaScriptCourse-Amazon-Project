2e0542b44c6b19287ea083c69849276febbb71a3
Tomari <tomari7878@gmail.com>
2024-09-18 13:12:46 -0500

    modified class version of cart

--- data/cart-oop.js
+++ data/cart-oop.js
@@ -41,24 +41,6 @@ function Cart(localStorageKey) {
       }
       this.saveToStorage();
     },
-    loadFromStorage() {
-      this.cartItems = JSON.parse(localStorage.getItem("cart"));
-
-      if (!this.cartItems) {
-        this.cartItems = [
-          {
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId: "1",
-          },
-          {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1,
-            deliveryOptionId: "2",
-          },
-        ];
-      }
-    },
 
     removeFromCart(productId) {
       const newCart = [];
