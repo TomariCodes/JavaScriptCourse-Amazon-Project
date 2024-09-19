4235f95f0850abc30a42ef52b93f177d88b2b505
Tomari <tomari7878@gmail.com>
2024-09-19 18:36:06 -0500

    made localStorageKey and loadFromStorage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,14 +1,14 @@
 class Cart {
   cartItems;
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey) {
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
   }
 
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
     if (!this.cartItems) {
       this.cartItems = [
@@ -27,7 +27,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productId) {
@@ -49,24 +49,7 @@ class Cart {
     }
     this.saveToStorage();
   }
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem("cart"));
 
-    if (!this.cartItems) {
-      this.cartItems = [
-        {
-          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-          quantity: 2,
-          deliveryOptionId: "1",
-        },
-        {
-          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-          quantity: 1,
-          deliveryOptionId: "2",
-        },
-      ];
-    }
-  }
   removeFromCart(productId) {
     const newCart = [];
 
@@ -94,6 +77,8 @@ class Cart {
 
 const cart = new Cart("cart-oop");
 const businessCart = new Cart("cart-business");
+
+
 console.log(cart);
 console.log(businessCart);
 
