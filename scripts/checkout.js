c0ee882c44654cdb58e15093f30e6ab64892b8c8
Tomari <tomari7878@gmail.com>
2024-09-26 16:39:22 -0500

    added promise class for promises and fetch lesson

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,17 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+
 // import "../data/cart-class.js";
 //import "../data/backend-practice.js";
+//promises are built in classes
+new Promise((resolve) => {
+  loadProducts(() => {
+resolve();
+  });
+}).then(() => {
+console.log('next step');
+});
 
 loadProducts(() => {
   renderOrderSummary();
