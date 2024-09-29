c70d7adcd8a484f2f7a214422d7b9bf924daca2e
Tomari <tomari7878@gmail.com>
2024-09-28 19:53:16 -0500

    Added a promise return statement

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,25 +1,35 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
 // import "../data/cart-class.js";
 //import "../data/backend-practice.js";
 //promises are built in classes
+
 new Promise((resolve) => {
-
   loadProducts(() => {
-    
-resolve();
+    resolve("value1");
   });
-}).then(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
-});
+})
+  .then((value) => {
+    console.log(value);
 
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  }).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
 
 /*
 loadProducts(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
 });
 */
