ebba45a88a3b14ab3d530ac36dbc9c2ca1aff3a1
Tomari <tomari7878@gmail.com>
2024-10-06 19:32:27 -0500

    added await to loadProductsFetch. Can now fetch product information

--- data/orders.js
+++ data/orders.js
@@ -1,148 +1,65 @@
 import { loadFromStorage, cart, loadCart } from "./cart.js";
 import {Product, products, loadProducts, getProduct, loadProductsFetch} from '../data/products.js'
 import { formatCurrency } from "../scripts/utils/money.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 export const orders = JSON.parse(localStorage.getItem('orders')) || [];
 
-loadProductsFetch();
+await loadProductsFetch();
 
 export function addOrder(order) {
 orders.unshift(order);
 saveToStorage();
 };
 
-
 function saveToStorage() {
     localStorage.setItem('orders', JSON.stringify(orders));
 }
-console.log(orders);
+
+let orderDetails = ``;
 
 
 orders.forEach((order) => {
+  const orderDate = dayjs(order.orderTime);
+  const orderDateFormat = orderDate.format('MMMM M');
 
-console.log(`Order ID: ${order.id}`);
-
-order.products.forEach((product) => {
-console.log(`Products Object:`, product);
-console.log(`Product Id: ${product.productId}`);
-const foundProduct = getProduct(product.productId);
-if (foundProduct) {
-  console.log(`Found Product:`, foundProduct);
-} else {
-  console.log(`Product with ID ${product.productId} not found in products array.`);
-}
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-  /*
-  let renderOrdersHTML = ``;
-  const orderedProducts = order.products;
-  orderedProducts.forEach ((product) => {
-
-console.log(product.productId);
-console.log(Product.id);
-
-console.log(product.productId === Product.id);
-
-
-  });
-
-  */
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function renderOrderHTML() {
-
-    renderOrdersHTML = `<div class="order-container-${order.id}">
+  orderDetails = `<div class="order-container-${order.id}">
     <div class="order-header">
-            <div class="order-header-left-section">
+    <div class="order-header-left-section">
               <div class="order-date">
                 <div class="order-header-label">Order Placed:</div>
-                <div>${order.orderTime.format('DDDD M')}</div>
+                <div>${orderDateFormat}</div>
                 </div>
               <div class="order-total">
               <div class="order-header-label">Total:</div>
-              <div>$${formatCurrency(order.TotalCostCents)}</div>
+              <div>$${formatCurrency(order.totalCostCents)}</div>
               </div>
               </div>
 
-            <div class="order-header-right-section">
+              <div class="order-header-right-section">
               <div class="order-header-label">Order ID:</div>
               <div>${order.id}</div>
-            </div>
-          </div>
+              </div>
+              </div>
+              
+              <div class="order-details-grid js-order-details-grid">`;
+  
+  order.products.forEach((product) => {
+  const estimatedDeliveryDate = dayjs(product.estimatedDeliveryDate);
+  const estimatedDeliveryDateFormat = estimatedDeliveryDate.format('MMMM M');
 
-          <div class="order-details-grid">
-            <div class="product-image-container">
-            <img src="images/products/athletic-cotton-socks-6-pairs.jpg" />
-            </div>
-
-            <div class="product-details">
-              <div class="product-name">
-                Black and Gray Athletic Cotton Socks - 6 Pairs
-                </div>
-              <div class="product-delivery-date">Arriving on: August 15</div>
-              <div class="product-quantity">Quantity: 1</div>
-              <button class="buy-again-button button-primary">
-              <img class="buy-again-icon" src="images/icons/buy-again.png" />
-              <span class="buy-again-message">Buy it again</span>
-              </button>
-            </div>
-
-            <div class="product-actions">
-              <a href="tracking.html">
-                <button class="track-package-button button-secondary">
-                  Track package
-                  </button>
-                </a>
-            </div>
-            <div class="product-image-container">
-            <img
-            src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg"
-            />
+  const foundProduct = getProduct(product.productId);
+  let orderedProducts = ``;
+  if (foundProduct) {
+    orderedProducts = `<div class="product-image-container">
+            <img src="${foundProduct.image}" />
             </div>
             
             <div class="product-details">
               <div class="product-name">
-                Adults Plain Cotton T-Shirt - 2 Pack
-              </div>
-              <div class="product-delivery-date">Arriving on: August 19</div>
-              <div class="product-quantity">Quantity: 2</div>
+                ${foundProduct.name}
+                </div>
+              <div class="product-delivery-date">Arriving on: ${estimatedDeliveryDateFormat} </div>
+              <div class="product-quantity">Quantity: ${product.quantity}</div>
               <button class="buy-again-button button-primary">
               <img class="buy-again-icon" src="images/icons/buy-again.png" />
               <span class="buy-again-message">Buy it again</span>
@@ -154,16 +71,11 @@ function renderOrderHTML() {
                 <button class="track-package-button button-secondary">
                   Track package
                   </button>
-              </a>
-              </div>
-              </div>
-        </div>`;
-    }
+                </a>
+                </div>`; 
+                
+                } 
+  });
 
-    
-   
-  
-    
+});
 
-    
-    
